62c5f05a1aff6bd07265f0d95b7206e090ff79cb
Nida Özçelik <nidaozcelik25@gmail.com>
2021-05-15 16:21:08 +0300

    configure reducers

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import configureStore from "./redux/reducers/configureStore"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = configureStore();
+
+ReactDOM.render(<Provider store={store}><App /> </Provider>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
