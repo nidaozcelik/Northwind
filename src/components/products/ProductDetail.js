e8fab7431abc8eca27ccce436af608d844b6245d
Nida Özçelik <nidaozcelik25@gmail.com>
2021-05-17 19:46:28 +0300

    not found added

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -5,7 +5,7 @@ import SelectInput from "../toolbox/SelectInput";
 const ProductDetail = ({ categories, product, onSave, onChange }) => {
     return (
         <form onSubmit={onSave}>
-            <h2>{product.id ? "Updated" : "Add"}</h2>
+            <h2>{product.id ? "Update" : "Add"}</h2>
             <TextInput
                 name="productName"
                 label="Product Name"
--- src/components/root/App.js
+++ src/components/root/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Dashboard";
 import CartDetail from "../cart/CartDetail";
 import { Route, Switch } from "react-router-dom"
 import AddOrUpdateProduct from "../products/AddOrUpdateProduct"
+import NotFound from "../common/NotFound"
 
 function App() {
   return (
@@ -13,9 +14,11 @@ function App() {
         <Navi />
         <Switch>
           <Route path="/" exact component={Dashboard} />
-          <Route path="/product" exact component={Dashboard} />
-          <Route path="/saveproduct/:productId" exact component={AddOrUpdateProduct} />
-          <Route path="/cart" exact component={CartDetail} />
+          <Route path="/product" component={Dashboard} />
+          <Route path="/saveproduct/:productId" component={AddOrUpdateProduct} />
+          <Route path="/saveproduct/" component={AddOrUpdateProduct} />
+          <Route path="/cart" component={CartDetail} />
+          <Route component={NotFound} />
         </Switch>
       </Container>
     </div>
