1eb688a708f77e00a964aa9266820e602a94130c
Nida Özçelik <nidaozcelik25@gmail.com>
2021-05-17 19:32:13 +0300

    product detail updated

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -25,6 +25,27 @@ const ProductDetail = ({ categories, product, onSave, onChange }) => {
                 onChange={onChange}
                 error="Error"
             />
+            <TextInput
+                name="quantityPerUnit"
+                label="Quantity Per Unit"
+                value={product.quantityPerUnit}
+                onChange={onChange}
+                error="Error"
+            />
+            <TextInput
+                name="unitPrice"
+                label="Unit Price"
+                value={product.unitPrice}
+                onChange={onChange}
+                error="Error"
+            />
+            <TextInput
+                name="unitsInStock"
+                label="Units In Stock"
+                value={product.unitsInStock}
+                onChange={onChange}
+                error="Error"
+            />
             <button type="submit" className="btn btn-success">Save</button>
         </form>
     )
